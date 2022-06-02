adbf4cbd8de3cb2a533f85070c0c4616888c32dd
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2022-06-02 21:19:10 +0430

    this file saves constant variables

---
+++ src/checkers/constant.py
@@ -0,0 +1,16 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+
+ROWS, COLUMNS = 8, 8
+
+SQUARE_SIZE = WIDTH // COLUMNS
+
+# rgb format
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
