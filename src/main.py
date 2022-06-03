cf49ab28791b564d2b415ebfa2318fb239ba7c6e
hamed-alemoni <67387096+hamed-alemoni@users.noreply.github.com>
2022-06-03 18:12:19 +0430

    get position from mouse when user click on each piece and move it to another square(just for now)

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import pygame
-from checkers.constant import WIDTH, HEIGHT
+from checkers.constant import WIDTH, HEIGHT, SQUARE_SIZE
 from checkers.board import Board
 
 
@@ -10,6 +10,13 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # name of game
 pygame.display.set_caption('Checkers')
+
+def get_row_column_from_mouse(position):
+
+    x, y = position
+    row = y // SQUARE_SIZE
+    column = x // SQUARE_SIZE
+    return row, column
 
 def main():
     run = True
@@ -27,9 +34,20 @@ def main():
                 run = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
-        board.draw_squares(WINDOW)
+
+                # get position when user click
+                position = pygame.mouse.get_pos()
+                # find row and column
+                row, column = get_row_column_from_mouse(position)
+                # get piece in there
+                piece = board.get_piece(row, column)
+                
+                board.move(piece, 4 , 3)
+
+        # make basic checkers window
+        board.draw_window(WINDOW)
         pygame.display.update()
+
     pygame.quit()
 
         
